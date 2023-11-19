8baadc325a46198d6506ae20f796f6975724ebb5
Nazim8115 <nazim.nn459@gmail.com>
2023-11-19 20:38:02 +0530

    write get all method

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from "express";
 const server = express();
 import productRouter from "./src/features/product/product.routes.js";
 server.use("api/products", productRouter);
+
 server.get("/", (req, res) => {
   res.send("welcome to  api");
 });
+
 server.listen(8500, () => {
   console.log("server is running at port 8500");
 });
